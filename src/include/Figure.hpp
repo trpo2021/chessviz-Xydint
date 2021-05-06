3f488dbd78923249dc5643f93f079202cffabf55
Xydint <Xydint@yandex.ru>
2021-05-06 20:22:11 +0700

    Add default constructor

--- src/include/Figure.hpp
+++ src/include/Figure.hpp
@@ -1,7 +1,8 @@
 class Figure {
 public:
-    enum Color { Black, White };
+    enum Color { Black, White, Not };
     enum Piece { King, Queen, Rook, Knight, Bishop, Pawn, None };
+    Figure();
     Figure(Color, Piece);
 
 private:
